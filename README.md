d03dc3f7b4b2107d81f800460b530976ee2c1d60
Thanawit Prasongpongchai <taepras@gmail.com>
2015-12-07 19:22:40 +0700

    restore project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # haii-cueng-web
+A web interface for rain prediction project using data mining approach between HAII (Thailand's Hydro and Agro Informatics Institute) and Faculty of Engineering, Chulalongkorn University.
---
+++ app/Http/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', 'PagesController@main');
+
+Route::get('d/{page}', function ($page) {
+   return view($page);
+});
